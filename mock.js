cda83f976980a7ed866261116757ea85ff3b8037
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-09-24 15:39:42 -0400

    findDuplicated

--- mock.js
+++ mock.js
@@ -21,5 +21,21 @@ function strOccurences(char, str) {
   return count;
 }
 
-console.log(strOccurences('l', 'hello'));
+// console.log(strOccurences('l', 'hello'));
 
+// FInd duplicate in an array of numbers
+function findDuplicated(arr) {
+  let sortedArr = arr.sort();
+  for (let i = 0; i < sortedArr.length - 1; i++) {
+    const element = sortedArr[i];
+    const next = sortedArr[i + 1];
+    if(element === next) {
+      return element;
+    }
+  }
+
+  return 'Sorry no repeated numbers';
+}
+
+const arr1 = [7, 24, 81, 32, 55, 1, 43, 99, -3, 43, 1230];
+console.log(findDuplicated(arr1));
