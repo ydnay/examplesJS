7201c66aa1b6ea8e7db34a02e25e5ebdd138f04e
Yandy Gonzalez <yandygonzalez@gmail.com>
2018-09-24 15:05:56 -0400

    strOccurrences

--- mock.js
+++ mock.js
@@ -2,10 +2,24 @@ function isPalindrome(str) {
   return str === str.split('').reverse().join('');
 }
 
-console.log(isPalindrome('level'));
-console.log(isPalindrome('lol'));
-console.log(isPalindrome('reinier'));
-console.log(isPalindrome('l'));
-console.log(isPalindrome(''));
-console.log(isPalindrome(' '));
-console.log(isPalindrome('palindrome'));
+// console.log(isPalindrome('level'));
+// console.log(isPalindrome('lol'));
+// console.log(isPalindrome('reinier'));
+// console.log(isPalindrome('l'));
+// console.log(isPalindrome(''));
+// console.log(isPalindrome(' '));
+// console.log(isPalindrome('palindrome'));
+
+function strOccurences(char, str) {
+  let count = 0;
+  for(let i = 0; i < str.length; i++) {
+    if(str[i] === char) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
+console.log(strOccurences('l', 'hello'));
+
